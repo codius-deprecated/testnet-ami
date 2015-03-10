c3d1918de49a03c4690d4abdfc7cde36470a44b5
Torrie Fischer <torrie@ripple.com>
2015-03-10 14:40:09 -0700

    Log testnet boot to a logfile

--- testnet-boot.sh
+++ testnet-boot.sh
@@ -1,2 +1,4 @@
-curl http://169.254.169.254/latest/user-data > /root/testnet-config.yml
-ansible-playbook -c local /root/testnet-boot.yml
+(
+  wget http://169.254.169.254/latest/user-data -O /root/testnet-config.yml
+  ansible-playbook -vvvv -c local /root/testnet-boot.yml
+) 2>&1 | tee /var/log/testnet_boot.log
